88f0d7eb8438019cb241617f8a542ec8873ceb51
Andrew Good <drewhgood@gmail.com>
2015-03-28 14:15:10 -0700

    empty fields added to test lat lon on game session.

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
+  $('#hunt_sb1 , #hunt_sb2').hide();
   $('#hide_form').click(function(){   
-    $('#myTabContentOverlay').fadeOut(550);
+    $('#myTabContentOverlay').fadeOut(350);
+    $('#content_mover').animate({"left": '+=15%'});
+    $('#hunt_sb1, #hunt_sb2').fadeIn(700);
   });
 });
 
